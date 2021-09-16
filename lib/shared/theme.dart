88adef3be5fcf8e566bc981eecb2eed3ff5312df
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-09-16 11:46:28 +0700

    Update Theme for global styling code

---
+++ lib/shared/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Noted Global Colors
+Color kPrimaryColor = const Color(0xFF5C40CC);
+Color kWhiteColor = const Color(0xFFFFFFFF);
+Color kBackGroundColor = const Color(0xFFFAFAFA);
+Color kDarkPurpleColor = const Color(0xFF1F1449);
+Color kGreyColor = const Color(0xFF9698A9);
+Color kInactiveColor = const Color(0xFFDBD7EC);
+Color kGreenColor = const Color(0xFF0EC3AE);
+Color kRedColor = const Color(0xFFEB70A5);
+Color kYellowColor = const Color(0xFFFFA235);
+
+// Noted Global TextStyle
+TextStyle tWhiteTextStyle = GoogleFonts.poppins(color: kWhiteColor);
+TextStyle tGreyTextStyle = GoogleFonts.poppins(color: kGreyColor);
+TextStyle tBlackTextStyle = GoogleFonts.poppins(color: kDarkPurpleColor);
+TextStyle tPurpleTextStyle = GoogleFonts.poppins(color: kPrimaryColor);
+TextStyle tGreenTextStyle = GoogleFonts.poppins(color: kGreenColor);
+TextStyle tRedTextStyle = GoogleFonts.poppins(color: kRedColor);
+
+// Noted GlobalFontWeight
+FontWeight light = FontWeight.w300;
+FontWeight normal = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
