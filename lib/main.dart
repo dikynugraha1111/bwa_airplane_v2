619762bde05b2c8cde5553aa4d80d0fada3e3879
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-09-17 18:52:28 +0700

    Add Splash Screen Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bwa_airplane_v2/views/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,10 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      
-      home: 
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter BWA AirPlane - 2.0 Null Safety',
+      home: SplashPage(),
     );
   }
 }
---
+++ lib/views/pages/splash_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+          child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 33.0),
+            decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/logo.png"))),
+          ),
+          Text(
+            "AIRPLANE",
+            style: tWhiteTextStyle.copyWith(fontSize: 32, letterSpacing: 10),
+          )
+        ],
+      )),
+    );
+  }
+}
